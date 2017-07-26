278a9e16cf1bf77e0f8350322e96fc3ea9867633
Bill Van Besien <bill@gigantum.io>
2017-07-26 16:09:35 -0400

    Added new test file for relay-based node queries

---
+++ lmsrvlabbook/tests/test_node_queries.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2017 FlashX, LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import pytest
+import tempfile
+import os
+import uuid
+import shutil
+from snapshottest import snapshot
+
+from graphene.test import Client
+import graphene
+from mock import patch
+
+from lmcommon.labbook import LabBook
+from lmcommon.configuration import Configuration
+
+from ..api import LabbookMutations, LabbookQuery
+
+
+# Create ObjectType clases, since the LabbookQueries and LabbookMutations are abstract (allowing multiple inheritance)
+class Query(LabbookQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(LabbookMutations, graphene.ObjectType):
+    pass
+
+
+@pytest.fixture()
+def mock_config_file():
+    """A pytest fixture that creates a temporary directory and a config file to match. Deletes directory after test"""
+    # Create a temporary working directory
+    temp_dir = os.path.join(tempfile.tempdir, uuid.uuid4().hex)
+    os.makedirs(temp_dir)
+
+    with tempfile.NamedTemporaryFile(mode="wt") as fp:
+        # Write a temporary config file
+        fp.write("""core:
+  team_mode: false 
+git:
+  backend: 'filesystem'
+  working_directory: '{}'""".format(temp_dir))
+        fp.seek(0)
+
+        # Create test client
+        schema = graphene.Schema(query=Query,
+                                 mutation=Mutation)
+
+        yield fp.name, temp_dir, schema  # name of the config file, temporary working directory, the schema
+
+    # Remove the temp_dir
+    shutil.rmtree(temp_dir)
+
+
+class TestLabBookServiceQueries(object):
+    def test_cats(self, mock_config_file, snapshot):
+        pass
