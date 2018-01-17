293cb4c2f265fea1e6bbf1ff9e4001f10b5ade60
Bill Van Besien <bill@gigantum.io>
2018-01-17 17:41:48 -0500

    Updated some snapshots

--- lmsrvlabbook/tests/snapshots/snap_test_environment_base_image_queries.py
+++ lmsrvlabbook/tests/snapshots/snap_test_environment_base_image_queries.py
@@ -45,6 +45,76 @@ snapshots['TestEnvironmentBaseImageQueries.test_get_available_base_images 1'] = 
                         ],
                         'url': None
                     }
+                },
+                {
+                    'node': {
+                        'componentId': 'ut-jupyterlab-1',
+                        'description': 'Unit Test 1',
+                        'developmentTools': [
+                            'jupyterlab'
+                        ],
+                        'dockerImageNamespace': 'gigdev',
+                        'dockerImageRepository': 'gm-quickstart',
+                        'dockerImageServer': 'hub.docker.com',
+                        'dockerImageTag': '9718fedc-2018-01-16',
+                        'icon': 'data:image/png;base64,<base64 encoded png image>',
+                        'id': 'QmFzZUNvbXBvbmVudDpnaWctZGV2X2NvbXBvbmVudHMyJnV0LWp1cHl0ZXJsYWItMSYw',
+                        'languages': [
+                            'python3'
+                        ],
+                        'license': 'MIT',
+                        'name': 'Unit Test1',
+                        'osClass': 'ubuntu',
+                        'osRelease': '16.04',
+                        'packageManagers': [
+                            'apt',
+                            'pip3'
+                        ],
+                        'readme': 'Empty for now',
+                        'repository': 'gig-dev_components2',
+                        'revision': 0,
+                        'tags': [
+                            'ubuntu',
+                            'python3',
+                            'jupyterlab'
+                        ],
+                        'url': None
+                    }
+                },
+                {
+                    'node': {
+                        'componentId': 'ut-jupyterlab-3',
+                        'description': 'Unit Test 3',
+                        'developmentTools': [
+                            'jupyterlab'
+                        ],
+                        'dockerImageNamespace': 'gigdev',
+                        'dockerImageRepository': 'gm-quickstart',
+                        'dockerImageServer': 'hub.docker.com',
+                        'dockerImageTag': '9718fedc-2018-01-16',
+                        'icon': 'data:image/png;base64,<base64 encoded png image>',
+                        'id': 'QmFzZUNvbXBvbmVudDpnaWctZGV2X2NvbXBvbmVudHMyJnV0LWp1cHl0ZXJsYWItMyYw',
+                        'languages': [
+                            'python3'
+                        ],
+                        'license': 'MIT',
+                        'name': 'Unit Test 3',
+                        'osClass': 'ubuntu',
+                        'osRelease': '16.04',
+                        'packageManagers': [
+                            'apt',
+                            'pip3'
+                        ],
+                        'readme': 'Empty for now',
+                        'repository': 'gig-dev_components2',
+                        'revision': 0,
+                        'tags': [
+                            'ubuntu',
+                            'python3',
+                            'jupyterlab'
+                        ],
+                        'url': None
+                    }
                 }
             ]
         }
@@ -64,7 +134,7 @@ snapshots['TestEnvironmentBaseImageQueries.test_get_available_base_images_pagina
                 }
             ],
             'pageInfo': {
-                'hasNextPage': False
+                'hasNextPage': True
             }
         }
     }
@@ -74,15 +144,49 @@ snapshots['TestEnvironmentBaseImageQueries.test_get_available_base_images_pagina
     'data': {
         'availableBases': {
             'edges': [
+                {
+                    'cursor': 'MQ==',
+                    'node': {
+                        'id': 'QmFzZUNvbXBvbmVudDpnaWctZGV2X2NvbXBvbmVudHMyJnV0LWp1cHl0ZXJsYWItMSYw',
+                        'name': 'Unit Test1'
+                    }
+                },
+                {
+                    'cursor': 'Mg==',
+                    'node': {
+                        'id': 'QmFzZUNvbXBvbmVudDpnaWctZGV2X2NvbXBvbmVudHMyJnV0LWp1cHl0ZXJsYWItMyYw',
+                        'name': 'Unit Test 3'
+                    }
+                }
             ],
             'pageInfo': {
-                'hasNextPage': False
+                'hasNextPage': True
             }
         }
     }
 }
 
 snapshots['TestEnvironmentBaseImageQueries.test_get_available_base_images_pagination_reverse 1'] = {
+    'data': {
+        'availableBases': {
+            'edges': [
+                {
+                    'cursor': 'Mw==',
+                    'node': {
+                        'id': 'QmFzZUNvbXBvbmVudDpnaWctZGV2X2NvbXBvbmVudHMyJnV0LWp1cHl0ZXJsYWItMiYw',
+                        'name': 'Unit Test 2'
+                    }
+                }
+            ],
+            'pageInfo': {
+                'hasNextPage': False,
+                'hasPreviousPage': True
+            }
+        }
+    }
+}
+
+snapshots['TestEnvironmentBaseImageQueries.test_get_available_base_images_pagination_reverse 2'] = {
     'data': {
         'availableBases': {
             'edges': [
