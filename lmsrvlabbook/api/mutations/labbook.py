b06baff770e81cd5d5c91f71533ff3ee2cad781d
Bill Van Besien <bill@gigantum.io>
2018-03-22 09:32:10 -0400

    Added lock - just in case

--- lmsrvlabbook/api/mutations/labbook.py
+++ lmsrvlabbook/api/mutations/labbook.py
@@ -407,10 +407,11 @@ class SetLabbookDescription(graphene.relay.ClientIDMutation):
         username = get_logged_in_username()
         lb = LabBook(author=get_logged_in_author())
         lb.from_name(username, owner, labbook_name)
-        lb.description = description_content
-        # Note - skipping activity record - That will be TODO
-        lb.git.add(os.path.join(lb.root_dir, '.gigantum/labbook.yaml'))
-        lb.git.commit('Updating description')
+        with lb.lock_labbook():
+            lb.description = description_content
+            # Note - skipping activity record - That will be TODO
+            lb.git.add(os.path.join(lb.root_dir, '.gigantum/labbook.yaml'))
+            lb.git.commit('Updating description')
         return SetLabbookDescription(success=True)
 
 
