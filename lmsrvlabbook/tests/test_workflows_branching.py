ddbca65ad70e22b9b72d952e8d149d4f68841127
Bill Van Besien <bill@gigantum.io>
2018-03-28 18:10:49 -0400

    Starting to flesh out tests

--- lmsrvlabbook/tests/test_workflows_branching.py
+++ lmsrvlabbook/tests/test_workflows_branching.py
@@ -19,40 +19,85 @@
 # SOFTWARE.
 import os
 import pytest
+import shutil
+import flask
+from flask import Flask
+import graphene
+from graphene.test import Client
+from mock import patch
 
-from snapshottest import snapshot
-from lmsrvlabbook.tests.fixtures import fixture_working_dir
+from lmcommon.auth.identity import get_identity_manager
 from lmcommon.configuration import Configuration
+from lmcommon.workflows import BranchManager
 from lmcommon.labbook import LabBook
 
-from graphene.test import Client
-import graphene
-from mock import patch
+from lmsrvcore.middleware import LabBookLoaderMiddleware, error_middleware
+from lmsrvlabbook.tests.fixtures import ContextMock, fixture_working_dir
+from lmsrvlabbook.api.query import LabbookQuery
+from lmsrvlabbook.api.mutation import LabbookMutations
+
+UT_USERNAME = "unittestbranchuser"
+UT_LBNAME = "unittest-workflow-branch-1"
 
 
 @pytest.fixture()
 def mock_create_labbooks(fixture_working_dir):
     # Create a labbook in the temporary directory
     lb = LabBook(fixture_working_dir[0])
-    lb.new(owner={"username": "default"}, name="labbook1", description="Cats labbook 1")
+    lb.new(owner={"username": UT_USERNAME}, name=UT_LBNAME, description="Cats labbook 1")
 
     # Create a file in the dir
-    with open(os.path.join(fixture_working_dir[1], 'sillyfile'), 'w') as sf:
-        sf.write("1234567")
+    with open(os.path.join(fixture_working_dir[1], 'unittest-examplefile'), 'w') as sf:
+        sf.write("test data")
         sf.seek(0)
     lb.insert_file('code', sf.name, '')
 
-    assert os.path.isfile(os.path.join(lb.root_dir, 'code', 'sillyfile'))
-    # name of the config file, temporary working directory, the schema
-    yield fixture_working_dir
+    assert os.path.isfile(os.path.join(lb.root_dir, 'code', 'unittest-examplefile'))
+
+    # Create test client
+    schema = graphene.Schema(query=LabbookQuery, mutation=LabbookMutations)
+    with patch.object(Configuration, 'find_default_config', lambda self: config_file):
+        app = Flask("lmsrvlabbook")
+        app.config["LABMGR_CONFIG"] = Configuration()
+        app.config["LABMGR_ID_MGR"] = get_identity_manager(Configuration())
+        with app.app_context():
+            flask.g.user_obj = app.config["LABMGR_ID_MGR"].get_user_profile()
+            client = Client(schema, middleware=[LabBookLoaderMiddleware(), error_middleware],
+                            context_value=ContextMock())
+            yield lb, client, schema
+    shutil.rmtree(fixture_working_dir, ignore_errors=True)
 
 
 class TestWorkflowsBranching(object):
-    def test_active_branch_name(self):
-        pass
+    def test_active_branch_name(self, mock_create_labbooks):
+        lb, client = mock_create_labbooks[0], mock_create_labbooks[1]
+        bm = BranchManager(lb, username=UT_USERNAME)
 
-    def test_available_branches(self):
-        pass
+        q = f"""
+        {{
+            labbook(name: "{UT_LBNAME}", owner: "{UT_USERNAME}") {{
+                activeBranchName
+            }}
+        }}
+        """
+        r = client.execute(q)
+        assert 'errors' not in r
+        assert r['data']['labbook']['activeBranchName'] == bm.active_branch
+
+    def test_available_branches(self, mock_create_labbooks):
+        lb, client = mock_create_labbooks[0], mock_create_labbooks[1]
+        bm = BranchManager(lb, username=UT_USERNAME)
+
+        q = f"""
+        {{
+            labbook(name: "{UT_LBNAME}", owner: "{UT_USERNAME}") {{
+                availableBranchNames
+            }}
+        }}
+        """
+        r = client.execute(q)
+        assert 'errors' not in r
+        assert r['data']['labbook']['activeBranchName'] == bm.active_branch
 
     def test_query_mergeable_branches_from_main(self):
         pass
